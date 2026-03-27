74b500327b3a1f4f967064d31d9acfb430535f59
happySquidd <rafael.safin077@gmail.com>
2026-03-27 16:52:57 -0400

    added live charts package and a graph to monitor hearbeat

--- views/models/PatientVitalsViewModel.cs
+++ views/models/PatientVitalsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,10 @@ namespace NolMed.views.models
 {
     public class PatientVitalsViewModel : BaseView
     {
-        private string _placeholderText;
+        public ChartValues<double> HeartRateValues { get; set; }
+        public SeriesCollection ChartSeries { get; set; }
 
+        private string _placeholderText;
         public string PlaceholderText
         {
             get => _placeholderText;
@@ -19,6 +23,43 @@ namespace NolMed.views.models
         public PatientVitalsViewModel()
         {
             PlaceholderText = "Patient Vitals Tab";
+            HeartRateValues = new ChartValues<double>();
+
+            ChartSeries = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Heart Rate",
+                    Values = HeartRateValues,
+                    PointGeometry = null,
+                    LineSmoothness = 0.5
+                }
+            };
+
+            SubscribeToHeartRate();
+        }
+
+        private void SubscribeToHeartRate()
+        {
+            // TODO: connect to redis
+            // Simulate heart rate updates
+            Task.Run(async () =>
+            {
+                Random rand = new Random();
+                while (true)
+                {
+                    // Simulate heart rate between 60-100
+                    double newHeartRate = 60 + rand.NextDouble() * 40;
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        HeartRateValues.Add(newHeartRate);
+                        // Keep only the latest 30 values
+                        if (HeartRateValues.Count > 30) HeartRateValues.RemoveAt(0);
+                    });
+                    // Update every second
+                    await Task.Delay(1000); 
+                }
+            });
         }
     }
 }
