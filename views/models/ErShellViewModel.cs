9210279ed847cccae41f32584ce958437cbb9642
happySquidd <rafael.safin077@gmail.com>
2026-04-14 20:25:39 -0400

    created a navigation shell for er room overview

---
+++ views/models/ErShellViewModel.cs
@@ -0,0 +1,31 @@
+﻿using NolMed.views.usercontrol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolMed.views.models
+{
+    public class ErShellViewModel : BaseView
+    {
+        public ERViewModel Section { get; }
+
+        private object _currentSubView;
+        public object CurrentSubView
+        {
+            get => _currentSubView;
+            set { _currentSubView = value; OnPropertyChanged(); }
+        }
+
+        public ErShellViewModel()
+        {
+            Section = new ERViewModel();
+        }
+
+        public static void RoomClicked(int roomNum)
+        {
+            
+        }
+    }
+}
